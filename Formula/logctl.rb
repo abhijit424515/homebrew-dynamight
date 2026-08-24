554d8cac534466947580fcbf0359111038afd794
Abhijit Kumar <abhijit424515@users.noreply.github.com>
2026-08-24 15:44:06 +0530

    logctl 0.3.0

--- Formula/logctl.rb
+++ Formula/logctl.rb
@@ -1,8 +1,8 @@
 class Logctl < Formula
   desc "Profile wrapper over logcli, driven by ~/logctl.toml"
   homepage "https://github.com/abhijit424515/logctl"
-  url "https://github.com/abhijit424515/logctl/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "8ba0ff367317d54c9637fa14c123db0c550c681ba0b835012acebb58aa82ee66"
+  url "https://github.com/abhijit424515/logctl/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "99600a76f73335bd030640169d733df180a93857c3dfbb965855b1e4041d8f69"
   license "MIT"
 
   depends_on "jq"
@@ -12,24 +12,18 @@ class Logctl < Formula
   def install
     bin.install "logctl"
     bin.install_symlink bin/"logctl" => "lc"
-    pkgshare.install "logctl.example.toml"
   end
 
   def caveats
     <<~EOS
-      Create ~/logctl.toml to define your profiles. An example config is at:
-        #{opt_pkgshare}/logctl.example.toml
+      Create your config with:
+        logctl init
     EOS
   end
 
   test do
-    (testpath/"c.toml").write <<~TOML
-      default = "dev"
-
-      [dev]
-      addr = "http://localhost:3100"
-    TOML
     ENV["LOGCTL_CONFIG"] = testpath/"c.toml"
+    system bin/"logctl", "init"
     assert_equal "dev", shell_output("#{bin}/logctl profiles").strip
     assert_match "LOKI_ADDR='http://localhost:3100'", shell_output("#{bin}/lc env")
   end
